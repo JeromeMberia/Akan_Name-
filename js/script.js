e706212784040f43495705c8782d3a6584840f44
Jerome Mberia <60657652+JeromeMberia@users.noreply.github.com>
2020-02-21 07:28:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function output() {
   var day = parseInt(document.getElementById("day").value);
   var year = parseInt(document.getElementById("year").value);
   var month = parseInt(document.getElementById("month").value);
-  var gender = parseInt(document.getElementById("gender").value);
+  var gender =document.getElementById("gender").value;
 
   var day = new Date(year + "/" + month + "/" + day);
   var birthday = day.getDay();
@@ -10,9 +10,13 @@ function output() {
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-  if (gender = "male") {
+  if (gender === "male") {
     alert('Your akan name is ' + maleNames[birthday]);
-  } else(gender = "female"); {
+  };
+    else if (gender === "female"); {
     alert('Your name is ' + femaleName[birthday]);
   };
+    else {
+      alert("enter a proper gender");
+  }
 }
